ee3f2c0f4d7d9408940c289cce6abc932e1fac6a
sarat-balogun <saratbalogun6@gmail.com>
2024-07-07 10:27:26 +0100

    Updates on javascript

---
+++ frontend/main.js
@@ -0,0 +1,20 @@
+window.addEventLisstner('DOMContentLoaded', (event) => {
+    getVisitCount();
+})
+
+const functionApi = "";
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then (response => {
+        console.log("website called function API.");
+        count = response.count
+        documet.getElemmentById("counter").innerText = count;
+
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
