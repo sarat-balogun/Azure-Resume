f87463e6fabe1b4f685601c3aeebff476d2fbd1e
sarat-balogun <saratbalogun6@gmail.com>
2024-07-28 13:36:22 +0100

    Deployed azure function

--- frontend/main.js
+++ frontend/main.js
@@ -1,12 +1,12 @@
 window.addEventLisstner('DOMContentLoaded', (event) => {
     getVisitCount();
 })
-
+const functionApiUrl = "https://saratgetresumecounter.azurewebsites.net/api/GetResumeCounter?code=7atAhfbdefBUk6luvf9MZdCQoJ72vcvKJWAQiy9uotK6AzFuOYH0tA%3D%3D"
 const functionApi = "http://localhost:7071/api/GetResumeCounter";
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then (response => {
         console.log("website called function API.");
